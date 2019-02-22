a143df79c98df880c90d90dc1e8cdacf3a17e79d
theryanclausen <theryanclausen@gmail.com>
2019-02-21 17:14:40 -0700

    ;

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -3,8 +3,15 @@
 import sys
 
 def making_change(amount, denominations):
-  pass 
-
+  if amount < 0:
+    return 0
+  cache = [0 for x in range(amount + 1)]
+  cache[0] = 1
+  for i in range(0, len(denominations)):
+    
+    for j in range(denominations[i], amount + 1):
+      cache[j] += cache[j - denominations[i]]
+  return cache[amount]  
 
 if __name__ == "__main__":
   # Test our your implementation from the command line
